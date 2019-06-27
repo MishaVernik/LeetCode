0ef11718a294d16374a2719065854a7d6ee0354c
MishaVernik <nodus1.5.3@gmail.com>
2019-06-27 23:11:59 +0300

    135

---
+++ Algorithms/N-ary Tree Postorder Traversal/solution.cpp
@@ -0,0 +1,44 @@
+/*Given an n-ary tree, return the postorder traversal of its nodes' values.
+
+For example, given a 3-ary tree:
+
+ 
+
+
+
+ 
+
+Return its postorder traversal as: [5,6,3,2,4,1].
+
+ 
+Note:
+
+Recursive solution is trivial, could you do it iteratively?
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+public:
+     vector<int> a;
+    vector<int> postorder(Node* root) {
+          
+        if (!root) return a;
+       
+        for(auto el : root->children){
+            postorder(el);
+        }
+         a.push_back(root->val);
+        return a;
+    }
+};
